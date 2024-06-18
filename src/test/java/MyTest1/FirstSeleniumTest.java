0af6cb17e25a759f0352918b1eff27d7ea9eff2a
Monali Mhetre <46853968+monalimhetre@users.noreply.github.com>
2024-06-18 14:58:55 -0400

    Update FirstSeleniumTest.java
    added chromedriver

--- src/test/java/MyTest1/FirstSeleniumTest.java
+++ src/test/java/MyTest1/FirstSeleniumTest.java
@@ -12,7 +12,7 @@ public class FirstSeleniumTest {
 	
 	public static void main(String [] args) {
 
-	System.setProperty("webdriver.chrome.driver","C:\\Users\\monal\\OneDrive\\Documents\\MyProject\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
 	
 	//driver.navigate().to("https://www.google.com/"); 
